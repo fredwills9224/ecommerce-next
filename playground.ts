2cf68b83b1e3dc914558c8b7c5cc6f92b5b08a5a
fredwills9224 <willsf9224@gmail.com>
2021-08-31 13:24:10 -0400

    passing functions validated data types

--- playground.ts
+++ playground.ts
@@ -1,3 +1,17 @@
 export default function play (){
-    console.log('Hello World');
+    
+    const name = 'Fred';
+    const age = 30;
+
+    function logPersonInfo(personName:string, personAge:number){
+
+        //const info = 'Name: ' +personName+ ', age: ' +personAge;
+        const info = `Name: ${personName}, age: ${personAge}`
+        console.log(info);
+        return info;
+
+    };
+
+    logPersonInfo(name, age);
+
 };
