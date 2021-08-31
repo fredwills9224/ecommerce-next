4e37016ed902660e26ccf66b5f83944a4797f497
fredwills9224 <willsf9224@gmail.com>
2021-08-31 13:57:21 -0400

    prop validation with classes + interfaces

--- playground.ts
+++ playground.ts
@@ -1,7 +1,26 @@
+// Validating [prop]'s [type] in [Person] interface
+    interface Person {
+        name: string
+        age: number
+    }
+// Validating [prop]'s [type] in [Person] interface
+
+// Validating [prop]'s [type] in [Person] object
+    // type Person = {
+    //     name: string
+    //     age: number
+    // };
+// Validating [prop]'s [type] in [Person] object
+
 export default function play (){
     
     const name = 'Fred';
     const age = 30;
+
+    const person : Person = {
+        name: 'Janay',
+        age: 34
+    };
 
     function logPersonInfo(personName:string, personAge:number){
 
@@ -12,6 +31,16 @@ export default function play (){
 
     };
 
+    function logPersonInfo2(person : Person){
+
+        //const info = 'Name: ' +personName+ ', age: ' +personAge;
+        const info = `Name: ${person.name}, age: ${person.age}`
+        console.log(info);
+        return info;
+
+    };
+
     logPersonInfo(name, age);
+    logPersonInfo2(person);
 
 };
