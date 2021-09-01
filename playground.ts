53369775201cca539a5501adcc459c50398edcba
fredwills9224 <willsf9224@gmail.com>
2021-08-31 20:27:23 -0400

    extends with interface

--- playground.ts
+++ playground.ts
@@ -1,15 +1,32 @@
+interface Person{
+    name: string;
+    age: number;
+};
+
+interface BusinessPerson extends Person{
+    salary: number;
+};
+
+interface AcademicPerson extends Person{
+    publications: string[];
+};
+
 export default function play (){
 
-    // const names: string[] = ['Fred', 'Janay'];
-    // const numbers: Array<number> = [1,2,3,4,5];
-    const random = Math.random() > 0.5 ? 'hello': [1,2]; 
-    // Continue if [random] is a string
-        if(typeof random === 'string'){
-            const upper = random.toUpperCase()
-        }else{
-            console.log(random);
-        }
-    // Continue if [random] is a string
-    console.log(random.length);
+    const person: AcademicPerson = {
+        name: 'fred',
+        age: 23,
+        publications: ['pub','pub1']
+    };
+    const person1: BusinessPerson = {
+        name: 'fred',
+        age: 23,
+        salary: 1000
+    };
+    function logPerson(person: Person){
+        console.log(person);
+    };
+    logPerson(person);
+    logPerson(person1);
 
 };
