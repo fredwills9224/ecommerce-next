60c4f28db7fc8322a3df354a30c3984fd570450c
fredwills9224 <willsf9224@gmail.com>
2021-09-01 19:08:12 -0400

    union narrowing with switch

--- playground.ts
+++ playground.ts
@@ -1,29 +1,60 @@
 type RaceCar = {
     name: string
-    maxSpeed: number
+    maxSpeed: 200,
+    team: string
 }
 
 type CityCar = {
     name: string,
-    space: string
+    space: string,
+    maxSpeed: 100
 };
+
+type Car = RaceCar | CityCar;
 
 export default function play (){
 
-    const blueCar: CityCar = {
-        name: 'blueCar',
-        space:'5seater'
-    }
-    const redCar: RaceCar = {
-        name: 'redCar',
-        maxSpeed: 140
+    const car: RaceCar = {
+        name: 'Race Car',
+        maxSpeed:200,
+        team: 'ferari'
     }
     // Union of arguments -> can only access attributes that both arguments share 
-        function logCarInfo(car: RaceCar | CityCar){
+        function logCarInfo(car: Car){
+            
             console.log(car.name);
+            switch(car.maxSpeed){
+                
+                case 200:
+                    console.log(car.team);
+                    break
+                ;
+                case 100:
+                    console.log(car);
+                    break
+                ;
+                default:
+                    console.log(car);
+                    break
+                ;
+
+            };
+
         };
-    // Union of arguments -> can only access attributes that both arguments share 
-    logCarInfo(blueCar);
-    logCarInfo(redCar);
+    // Union of arguments -> can only access attributes that both arguments share
+
+    logCarInfo(car);
 
 };
+
+
+
+
+
+
+
+
+
+
+
+
