68b9a604f68b9ca0af2251a1b898b574718b4bad
fredwills9224 <willsf9224@gmail.com>
2021-08-31 21:24:48 -0400

    extends with type

--- playground.ts
+++ playground.ts
@@ -1,5 +1,5 @@
 interface Person{
-    name: string;
+    personName: string;
     age: number;
 };
 
@@ -11,15 +11,27 @@ interface AcademicPerson extends Person{
     publications: string[];
 };
 
+type Car = {
+    carName: string
+};
+
+type RaceCar = {
+    speed: number
+} & Car & { bestLap: string } & BusinessPerson;
+
+type DragRaceCar = {
+    parashootDeployed: boolean 
+} & Car & RaceCar;
+
 export default function play (){
 
     const person: AcademicPerson = {
-        name: 'fred',
+        personName: 'fred',
         age: 23,
         publications: ['pub','pub1']
     };
     const person1: BusinessPerson = {
-        name: 'fred',
+        personName: 'fred',
         age: 23,
         salary: 1000
     };
@@ -28,5 +40,27 @@ export default function play (){
     };
     logPerson(person);
     logPerson(person1);
+    const car: RaceCar = {
+        carName: 'my car',
+        speed: 100,
+        bestLap: '01:17:02',
+        personName: 'my race car driver',
+        salary: 2000,
+        age: 24
+    };
+    const car1: DragRaceCar = {
+        carName: 'my car1',
+        speed: 170,
+        bestLap: '00:58:67',
+        personName: 'my drag-race car driver',
+        salary: 4000,
+        age: 29,
+        parashootDeployed: false
+    };
+    function logCar(car: Car){
+        console.log(car);
+    };
+    logCar(car);
+    logCar(car1);
 
 };
