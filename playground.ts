b1677484a31428582ebea1a62f97051f512e02e7
fredwills9224 <willsf9224@gmail.com>
2021-08-31 22:33:33 -0400

    Uniions

--- playground.ts
+++ playground.ts
@@ -1,66 +1,29 @@
-interface Person{
-    personName: string;
-    age: number;
-};
-
-interface BusinessPerson extends Person{
-    salary: number;
-};
-
-interface AcademicPerson extends Person{
-    publications: string[];
-};
-
-type Car = {
-    carName: string
-};
-
 type RaceCar = {
-    speed: number
-} & Car & { bestLap: string } & BusinessPerson;
+    name: string
+    maxSpeed: number
+}
 
-type DragRaceCar = {
-    parashootDeployed: boolean 
-} & Car & RaceCar;
+type CityCar = {
+    name: string,
+    space: string
+};
 
 export default function play (){
 
-    const person: AcademicPerson = {
-        personName: 'fred',
-        age: 23,
-        publications: ['pub','pub1']
-    };
-    const person1: BusinessPerson = {
-        personName: 'fred',
-        age: 23,
-        salary: 1000
-    };
-    function logPerson(person: Person){
-        console.log(person);
-    };
-    logPerson(person);
-    logPerson(person1);
-    const car: RaceCar = {
-        carName: 'my car',
-        speed: 100,
-        bestLap: '01:17:02',
-        personName: 'my race car driver',
-        salary: 2000,
-        age: 24
-    };
-    const car1: DragRaceCar = {
-        carName: 'my car1',
-        speed: 170,
-        bestLap: '00:58:67',
-        personName: 'my drag-race car driver',
-        salary: 4000,
-        age: 29,
-        parashootDeployed: false
-    };
-    function logCar(car: Car){
-        console.log(car);
-    };
-    logCar(car);
-    logCar(car1);
+    const blueCar: CityCar = {
+        name: 'blueCar',
+        space:'5seater'
+    }
+    const redCar: RaceCar = {
+        name: 'redCar',
+        maxSpeed: 140
+    }
+    // Union of arguments -> can only access attributes that both arguments share 
+        function logCarInfo(car: RaceCar | CityCar){
+            console.log(car.name);
+        };
+    // Union of arguments -> can only access attributes that both arguments share 
+    logCarInfo(blueCar);
+    logCarInfo(redCar);
 
 };
