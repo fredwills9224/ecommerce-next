60f910dee2bf08d388ad5247f1e061b3008f3104
fredwills9224 <willsf9224@gmail.com>
2021-09-04 00:23:41 -0400

    generic array

--- playground.ts
+++ playground.ts
@@ -21,19 +21,19 @@ interface AcademicPerson extends Person {
     };
 // Declaration merging can only be performed on interfaces
 
-type Noop = ()=> any;
-type Noop2 = ()=> void;
+
 
 export default function play (){
 
-    function fn1(x: Noop): any{
-        const result = x();
-        result();
-    };    
-    function fn2(x: Noop2): void{
-        const result = x();
-        // result(); {'error': 'Expression not callable. Void has no call signatures.'}
-    };    
+    function iterate(items: Array<string>){
+
+        items.forEach((item)=>{
+            console.log(item.toUpperCase());
+        });
+
+    };
+
+    iterate(['fred', 'janay', 'harmony',]);
 
 };
 
