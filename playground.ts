786482a6264b86667d0c7ef870ec9519ad0d62ec
fredwills9224 <willsf9224@gmail.com>
2021-09-04 23:20:20 -0400

    single type array

--- playground.ts
+++ playground.ts
@@ -1,165 +1,47 @@
-interface Person {
-    name: string,
+interface Person{
+    name: string
 };
 
-interface Student extends Person{
-    age: number,
-    city: string
-};
-
-interface BusinessPerson extends Student{
-    car: string
-};
-
-interface PostGraduateStudent extends Student{
-    projects: string[]
-};
-
-type StudentInfo<T extends Student = Student> ={
-    data: T,
-    grades: number[],
-    internships?: string[]
-};
-
-// interface StudentInfo<T extends Student = Student> {
-//     data: T,
-//     grades: number[],
-//     internships?: string[]
-// };
-
-type BusinessPersonInfo = {
-    data: BusinessPerson,
-    grades: number[],
-    salary: number
-};
-
-type StudentInfoAnyType<T extends any = Student> = 
-    T extends Student ? 
-    { 
-        data: T, 
-        grades: number[], 
-        internships?: string[] 
-    } : string
+type SingleType<T> = 
+    T extends any[] ?
+    T[number] : T
 ;
 
-type Dropout = { recevedDiploma: boolean }
+type Type = SingleType<string[]>;
+type Type1 = SingleType<number[]>;
+type Type2 = SingleType<Person[]>;
 
 export default function play (){
 
-    function logStudentInfo(info: StudentInfo){
-        console.log(info.data.name);
-        console.log(info.data.age);
-        console.log(info.data.city);
-        console.log(info.grades);
-        if(info.internships){
-            console.log(info.internships);
-        }
+    const stringList = ['1', '2', '3'];
+    const numberList = [1, 2, 3];
+    const personList = [
+        {name: '1'},
+        {name: '2'},
+        {name: '3'}
+    ];
+    const mixList = ['1', 2, {name: '3'}];
+ 
+    function logStringArray(arrayInfo: Type[]){
+        console.log(arrayInfo);
     };
-
-    function logStudentInfoAnyTypeDropoutDefault(info: StudentInfoAnyType<Dropout>){
-        console.log(info);
+    function logNumberArray(arrayInfo: Type1[]){
+        console.log(arrayInfo);
     };
-
-    function logStudentInfoAnyTypeStudentDefault(info: StudentInfoAnyType){
-        console.log(info.data.name);
-        console.log(info.data.age);
-        console.log(info.data.city);
-        console.log(info.grades);
-        console.log(info.internships);
-    };
-
-    function logPostGraduateStudentInfo(info: StudentInfo<PostGraduateStudent>){
-        
-        console.log(info.data.name);
-        console.log(info.data.age);
-        console.log(info.data.city);
-        console.log(info.data.projects);
-        console.log(info.grades);
-        if(info.internships){
-            console.log(info.internships);
-        }
-
+    function logPersonArray(arrayInfo: Type2[]){
+        console.log(arrayInfo);
     };
     
-    function logBusinessPersonInfo(info: BusinessPersonInfo){
-        console.log(info.data.name);
-        console.log(info.data.age);
-        console.log(info.data.city);
-        console.log(info.data.car);
-        console.log(info.grades);
-        console.log(info.salary)
-    };
 
-    const studentInfo = {
-
-        data: {
-            name: 'harmony',
-            age: 15,
-            city: 'Cincinnati'
-        },
-        grades: [4, 4, 4, 4]
-
-    };
-    const student1Info = {
-
-        data: {
-            name: 'harmony2',
-            age: 15,
-            city: 'Cincinnati'
-        },
-        grades: [4, 4, 4, 4],
-        internships: ['google', 'facebook']
-
-    };
-
-    const studentInfoForDropout = 
-        'student records incomplete'
-    ;
-
-    const objectiveStudent = {
-        
-        data: {
-            name: 'Unity',
-            age: 7,
-            city: 'Cincinnati'
-        },
-        grades: [4, 4, 4, 4],
-        internships: []
-        
-    };
+    logStringArray(stringList);
+    logNumberArray(numberList);
+    logPersonArray(personList);
     
-    const postGraduateStudent = {
-
-        data: {
-            name: 'janay',
-            age: 15,
-            city: 'Cincinnati',
-            projects: ['Unlimited', 'Flowers over Stones', 'Phoenix']
-        },
-        grades: [4, 4, 4, 4],
-        internships: ['google', 'facebook'],
-
-    };
-
-    const businessPersoninfo = {
-
-        data: {
-            name: 'fred',
-            age: 34,
-            city: 'Cincinnati',
-            car: 'something nice'
-        },
-        grades: [4, 3.8, 3.9, 3.9],
-        salary: 100000
-
-    };
-
-    logBusinessPersonInfo(businessPersoninfo);
-    logStudentInfo(studentInfo);
-    logStudentInfo(student1Info);
-    logPostGraduateStudentInfo(postGraduateStudent);
-    logStudentInfoAnyTypeDropoutDefault(studentInfoForDropout);
-    logStudentInfoAnyTypeStudentDefault(objectiveStudent);
+    // mehtods not allowed
+        // logStringArray(mixList);
+        // logNumberArray(mixList);
+        // logPersonArray(mixList);
+    // mehtods not allowed
 
 };
 
