ab75bde66333403b1f9968d224132bcd505261b6
fredwills9224 <willsf9224@gmail.com>
2021-09-05 11:38:01 -0400

    mapped types

--- playground.ts
+++ playground.ts
@@ -1,49 +1,23 @@
-interface Person{
+interface Driver{
     name: string
 };
 
-// Generic [CustomArray] default [T]ype set to [string]
-    type CustomArray<T = string> = {
-        [index: number]: T,
-        push: Function
+// [CustomObject] with [key] value pairs
+    type CustomObject = {
+        [key: string]: string | number | Driver
     };
-// Generic [CustomArray] default [T]ype set to [string]
+// [CustomObject] with [key] value pairs
 
-// [SingleStringTypeFromCustomArray] type from [CustomArray]
-    type SingleStringTypeFromCustomArray = CustomArray<string>[number];
-// [SingleStringTypeFromCustomArray] type from [CustomArray]
-// [SinglePersonTypeFromCustomArray] type from [CustomArray]
-    type SinglePersonTypeFromCustomArray = CustomArray<Person>[number];
-// [SinglePersonTypeFromCustomArray] type from [CustomArray]
-// [SingleNumberTypeFromCustomArray] type from [CustomArray]
-    type SingleNumberTypeFromCustomArray = CustomArray<number>[number];
-// [SingleNumberTypeFromCustomArray] type from [CustomArray]
 
-// Array index [T]ype initialized by default setting of [CustomArray] 
-    const stringItems: CustomArray = ['1', '2', '3'];
-// Array index [T]ype initialized by default setting of [CustomArray] 
-// Array index [T]ype initialized by [CustomArray<Person>] 
-    const personItems: CustomArray<Person> = [{name: 'janay'}, {name: 'harmony'}];
-// Array index [T]ype initialized by [CustomArray<Person>] 
-// Array index [T]ype initialized by [CustomArray<number>] 
-    const numberItems: CustomArray<number> = [1, 2, 3];
-// Array index [T]ype initialized by [CustomArray<number>] 
-
-// Strictly declaring varaible with [T]ype that matches [CustomArray<T>]
-    const stringItem: SingleStringTypeFromCustomArray = '4';
-    const personItem: SinglePersonTypeFromCustomArray = {name: 'fred'};
-    const numberItem: SingleNumberTypeFromCustomArray = 4;
-// Strictly declaring varaible with [T]ype that matches [CustomArray<T>]
+const car: CustomObject = {
+    age: 4,
+    driver: {name:'fred'},
+    city: 'Cincinnati'
+};
 
 export default function play (){
     
-    stringItems.push(stringItem);
-    personItems.push(personItem);
-    numberItems.push(numberItem);
-
-    console.log(stringItems);
-    console.log(personItems);
-    console.log(numberItems);
+    console.log(car);
 
 };
 
