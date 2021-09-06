42d87a924686ae1da8989ebd3c593c0e11d6e4cf
fredwills9224 <willsf9224@gmail.com>
2021-09-05 20:22:51 -0400

    key of

--- playground.ts
+++ playground.ts
@@ -1,81 +1,56 @@
-// matching return types
-    type ReturnType<T> = 
-        T extends T ? 
-        T : unknown
-    ;
-// matching return types
-// matching return types with infer
-    // type ReturnType<T> =
-    //     T extends infer R ?
-    //     R : unknown
-    // ;
-// matching return types with infer
-//type ReturnType<T> = T extends ()=> infer R? R: number;
+type MotherKey = keyof Mother;
+type FatherKey = keyof Father;
 
-// original return types
-    function loggerType(){
-        return 'hello world';
-    };
-    function logger1Type(){
-        return 2;
-    };
-    function add2TypeLogger(a:number){
-        a += 2;
-        return a;
-    };
-    function addParametersTypeLogger(a1: number, b: number){
-        const c:number = a1 + b
-        return c;
-    };
-// original return types
+interface Mother{
+    name: string,
+    age: number,
+    chromosoneX: MotherKey
+};
+interface Father{
+    name: string,
+    age: number,
+    chromosoneX: MotherKey,
+    chromosoneY: FatherKey
+};
 
-// return types matched once
 
-    const loggerReturnLoggerType: ReturnType <typeof loggerType> = 
-        ()=> 'hi there'
-    ;
-    const loggerReturnLogger1Type: ReturnType <typeof logger1Type> = 
-        ()=> 22;
-    ;
-    const loggerReturnAdd2TypeLogger: ReturnType <typeof add2TypeLogger> = 
-        
-        (a2: number)=>{
-            a2 += 2
-            return a2;
-        }
+interface ChildPerson{
+    name: string,
+    age: number,
+    motherKey: MotherKey,
+    fatherKey: FatherKey
+};
 
-    ;
-    const loggerReturnAddParametersTypeLogger: ReturnType<typeof addParametersTypeLogger> =
-        
-        (a3: number, b1:number)=>{
-            const c1:number = a3 + b1;
-            return c1;
-        }
+let motherKey: MotherKey = 'chromosoneX';
+let fatherKey: FatherKey = 'chromosoneY';
 
-    ;
-    
-// return types matched once
+const mother: Mother = {
+    name: 'janay',
+    age: 33,
+    chromosoneX: 'chromosoneX'
+};
 
-// return types matched twice
-    const addParameters: typeof loggerReturnAddParametersTypeLogger = (a4:number, b2:number)=>{
-        const c2:number = a4 + b2;
-        return c2;
-    };
-// return types matched twice
+const father: Father = {
+    name: 'fred',
+    age: 34,
+    chromosoneX: 'chromosoneX',
+    chromosoneY: 'chromosoneY'
+};
+
+const child: ChildPerson = {
+    name: 'harmony',
+    age: 15,
+    motherKey: 'chromosoneX',
+    fatherKey: 'chromosoneX'
+}; 
 
 export default function play(){
 
-    console.log(loggerType());
-    console.log(logger1Type());
-    console.log(add2TypeLogger(1));
-    console.log(addParametersTypeLogger(2,3));
-
-    console.log(loggerReturnLoggerType());
-    console.log(loggerReturnLogger1Type());
-    console.log(loggerReturnAdd2TypeLogger(4));
-    console.log(loggerReturnAddParametersTypeLogger(5,6));
-    
-    console.log(addParameters(7,8));
+    console.log(mother);
+    console.log(father);
+    console.log(child);
+    console.log(motherKey);
+    console.log(fatherKey);
 
 };
 
