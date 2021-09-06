2e00ac5e1087674bed2d057a85115192c08457e9
fredwills9224 <willsf9224@gmail.com>
2021-09-06 01:31:10 -0400

    infer get static props

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,24 @@
-import { useEffect } from "react";
-import play from "../playground";
+import type { InferGetStaticPropsType } from 'next';
 
-export default function Home() {
+export async function getStaticProps(){
 
-  useEffect(()=> {
-    play();
-  }, []);
+  const products = [1, 2, 3];
+  return{
+    props:{
+      products
+    },
+    // Check database every 4 hours for new products
+      revalidate: 4 * 60 * 60
+    // Check database every 4 hours for new products
+  };
+
+};
+
+export default function Home({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
     <div>
-      Hello World
+      { products }
     </div>
   );
 
