406005db7537bb7ebc11d5bb40f1f688dfd00a55
fredwills9224 <willsf9224@gmail.com>
2021-09-09 16:40:13 -0400

    normalize product images

--- framework/shopify/utils/normalize.ts
+++ framework/shopify/utils/normalize.ts
@@ -1,11 +1,25 @@
-import { Product as ShopifyProduct } from "../schema";
+import { ImageEdge, Product as ShopifyProduct } from "../schema";
+
+function normalizeProductImages({edges}: {edges: Array<ImageEdge>}){
+
+    return edges.map(({ node: { originalSrc: url, ...rest } })=>{
+
+        return {
+            url: `/images/${url}`,
+            ...rest
+        };
+
+    });
+
+};
 
 export function normalizeProduct(productNode: ShopifyProduct): any{
 
     // Destructuring [productNode] properties
         const {
             id, title: name, handle,
-            vendor, description, ...rest
+            vendor, description, images: imageConnection,
+            ...rest
         } = productNode;
     // Destructuring [productNode] properties
 
@@ -14,6 +28,7 @@ export function normalizeProduct(productNode: ShopifyProduct): any{
             id, name, vendor,
             description, path: `/${handle}`,
             slug: handle.replace(/^\/+|\/+$/g, ''),
+            images: normalizeProductImages(imageConnection),
             ...rest
         };
     // Creating new product from [productNode] properties
