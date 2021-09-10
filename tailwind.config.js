0fbd0ba887d058719e976be9e6ca74858a8c2add
fredwills9224 <willsf9224@gmail.com>
2021-09-10 14:58:53 -0400

    tailwind & postcss

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+
+    purge: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './componenets/**/*.{js,ts,jsx,tsx}'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+
+};
