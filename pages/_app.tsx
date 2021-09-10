a31e1bddc0e73d4d9b20c10afcefdeade317621d
fredwills9224 <willsf9224@gmail.com>
2021-09-10 12:43:55 -0400

    Layout component

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,16 @@
 import { AppProps } from 'next/app';
+import { Layout } from '@components/common';
 
 function MyApp({ Component, pageProps }: AppProps) {
-
     return (
         <>
-            <Component {...pageProps} />
+
+            <Layout>
+                <Component {...pageProps} />
+            </Layout>
+
         </>
     );
-
 };
 
 export default MyApp;
