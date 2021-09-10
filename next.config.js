3aa461712e33cdaac6325ca7098e83fb7c2fb9fd
fredwills9224 <willsf9224@gmail.com>
2021-09-09 21:36:06 -0400

    framework specific configuration

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
-module.exports = {
+const { withFrameworkConfig } = require('./framework/common/config');
+
+module.exports = withFrameworkConfig({
   i18n: { 
     locales: ['en-US', 'es'],
     defaultLocale: 'en-US'
   },
   //reactStrictMode: true,
-}
+});
 
 console.log('next.config.js', JSON.stringify(module.exports, null, 2));
