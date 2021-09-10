5dd765bd03fed6f2b93eca11e828ead491f64c01
fredwills9224 <willsf9224@gmail.com>
2021-09-10 01:01:05 -0400

    handle corner cases in config1

--- framework/common/config.js
+++ framework/common/config.js
@@ -3,9 +3,25 @@ const fs = require('fs');
 const merge = require('deepmerge');
 const prettier = require('prettier');
 
+const ALLOWED_FW = ['shopify', 'bigcommerce', 'shopify_local'];
+const FALLBACK_FW = 'shopify';
+
 function withFrameworkConfig(defaultConfig = {}){
 
-    const framework = defaultConfig?.framework.name;
+    let framework = defaultConfig?.framework?.name;
+    if(!framework){
+        throw new Error('The api framework is missing, please add a valid provider!');
+    }
+    if(!ALLOWED_FW.includes(framework)){
+
+        throw new Error(
+            `The api framework: ${framework} can\'t be found, please use one of the following ${ALLOWED_FW.join(', ')}`
+        );
+
+    }
+    if(framework === 'shopify_local'){
+        framework = FALLBACK_FW;
+    }
     const frameworkNextConfig = require(path.join('../', framework, 'next.config'));
     const config = merge(defaultConfig, frameworkNextConfig);
     const tsPath = path.join(process.cwd(), 'tsconfig.json');
