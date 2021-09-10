435f003b8e7988b89b24d823718d6f5beddec849
fredwills9224 <willsf9224@gmail.com>
2021-09-09 21:06:38 -0400

    next config file

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-  reactStrictMode: true,
+  i18n: { 
+    locales: ['en-US', 'es'],
+    defaultLocale: 'en-US'
+  },
+  //reactStrictMode: true,
 }
+
+console.log('next.config.js', JSON.stringify(module.exports, null, 2));
