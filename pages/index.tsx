9060a3f071abe4c226205a11f3a8a66219acb6bf
fredwills9224 <willsf9224@gmail.com>
2021-09-10 21:17:03 -0400

    Layout styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,7 +21,7 @@ export async function getStaticProps(){
 export default function Home({ products }: InferGetStaticPropsType<typeof getStaticProps>) {
 
   return (
-    <div>
+    <div className='root'>
       { JSON.stringify(products) }
     </div>
   );
