fd12e268bbae5b5554c0179192580d0be26af29d
fredwills9224 <willsf9224@gmail.com>
2021-09-10 23:18:43 -0400

    css variables

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,12 +1,17 @@
 import { FC } from 'react';
-import style from './Layout.module.css';
+import s from './Layout.module.css';
 
 const Layout: FC  = ({ children })=> {
 
     return(
-        <div className={style.root}>
-            <main className ='fit'>
+        <div className={s.root}>
+            <main
+                style={{color: 'var(--primary)'}} 
+                className ='fit'
+            >
+
                 { children }
+
             </main>
         </div>
     );
