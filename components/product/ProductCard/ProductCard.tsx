5911049ecde4c10406a4d1d482947406726318ee
fredwills9224 <willsf9224@gmail.com>
2021-09-13 12:37:10 -0400

    scaling up

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -29,6 +29,7 @@ const ProductCard: FC<Props> = ({ product })=> {
                 </div>
                 {product.images &&(
                     <Image
+                        className={s.productImage}
                         alt={product.name ?? 'Product image'}
                         src={product.images[0].url ?? placeholderImage}
                         height={540}
