26934dc2fa315384c64f7c233057605bb250994f
fredwills9224 <willsf9224@gmail.com>
2021-09-16 10:37:29 -0400

    hero component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import getAllProducts from '@framework/product/get-all-products';
 import { getConfig } from '@framework/api/config';
 import { Layout } from '@components/common';
 import { ProductCard } from '@components/product';
-import { Grid } from '@components/ui';
+import { Grid, Hero } from '@components/ui';
 
 export async function getStaticProps(){
 
@@ -35,6 +35,10 @@ export default function Home({ products }: InferGetStaticPropsType<typeof getSta
 
         )}
       </Grid>
+      <Hero
+        headline='Cookies, ice cream and muffin'
+        description='Bear claw candy macaroon cotton candy bear claw pudding brownie marshmallow marzipan. Cheesecake lemon drops muffin gingerbread candy canes cake tart. Cotton candy gingerbread carrot cake gummies sweet roll chocolate bar. Caramels croissant shortbread cotton candy toffee halvah soufflé apple pie sweet roll. Candy gummi bears caramels topping oat cake. Cotton candy fruitcake halvah marshmallow sesame snaps.'
+      />
 
     </>
   );
