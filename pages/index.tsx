b54bb89f0f36e8038597ab6cb87d971d2ddcd533
fredwills9224 <willsf9224@gmail.com>
2021-09-17 00:13:08 -0400

    layout B

--- pages/index.tsx
+++ pages/index.tsx
@@ -54,6 +54,18 @@ export default function Home({ products }: InferGetStaticPropsType<typeof getSta
         )}
 
       </Marquee>
+      <Grid layout='B'>
+
+        {products.slice(0,3).map(product => 
+
+          <ProductCard 
+            key={product.id}
+            product={product}
+          />
+
+        )}
+
+      </Grid>
 
     </>
   );
