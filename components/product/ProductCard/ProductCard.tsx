64f91511037b4716d3160d511090bfd529a26b0f
fredwills9224 <willsf9224@gmail.com>
2021-09-16 22:02:13 -0400

    slim Product

--- components/product/ProductCard/ProductCard.tsx
+++ components/product/ProductCard/ProductCard.tsx
@@ -17,7 +17,28 @@ const ProductCard: FC<Props> = ({ product, variant = 'simple' })=> {
         <Link href={`/products/${product.slug}`}>
             <a className={s.root}>
 
-                {variant === 'slim' ? <>Slim Product</> : (
+                {variant === 'slim' ? 
+                    <>
+                        
+                        <div className='inset-0 flex items-center justify-center absolute z-20'>
+                            <span className='bg-black  text-white p-3 font-bold text-xl'>
+                                {product.name}
+                            </span>
+                        </div>
+                        {product.images &&(
+                            <Image
+                                className={s.productImage}
+                                alt={product.name ?? 'Product image'}
+                                src={product.images[0].url ?? placeholderImage}
+                                height={320}
+                                width={320}
+                                quality='85'
+                                layout='fixed'
+                            />
+                        )}
+
+                    </> 
+                    : (
                     <>
 
                         <div className={s.productBg}></div>
--- components/ui/Marquee/Marquee.tsx
+++ components/ui/Marquee/Marquee.tsx
@@ -8,7 +8,11 @@ interface Props{
 const Marquee: FC<Props> = ({ children })=>{
     return(
         <div className={s.root}>
-            {children}
+
+            <div className={s.container}>
+                {children}
+            </div>
+
         </div>
     );
 };
