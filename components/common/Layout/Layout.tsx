0ced668b29ba849085a680f005e59e97062c3652
fredwills9224 <willsf9224@gmail.com>
2021-09-19 03:54:23 -0400

    sidebars, wishlist

--- components/common/Layout/Layout.tsx
+++ components/common/Layout/Layout.tsx
@@ -1,5 +1,7 @@
 import { FC } from 'react';
 import { Footer, Navbar } from '@components/common';
+import { Sidebar } from '@components/ui';
+import { CartSidebar } from '@components/cart';
 import s from './Layout.module.css';
 
 const Layout: FC  = ({ children })=> {
@@ -7,6 +9,9 @@ const Layout: FC  = ({ children })=> {
     return(
         <div className={s.root}>
             <Navbar/>
+            <Sidebar>
+                <CartSidebar/>
+            </Sidebar>
             <main className ='fit'>
                 { children }
             </main>
