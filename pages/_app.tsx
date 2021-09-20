d3c6015df1ede13f9dfc4cd60ce6900ced1724a7
fredwills9224 <willsf9224@gmail.com>
2021-09-20 01:32:16 -0400

    disable scroll

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@assets/main.css';
 import { AppProps } from 'next/app';
 import { FC } from 'react';
-import { UIProvider, useUI } from '@components/ui/Context';
+import { UIProvider } from '@components/ui/Context';
 
 // wrapper [FC] that has no operation other than passing [children]
     const Noop: FC = ({ children })=> <>{children}</>
@@ -12,8 +12,6 @@ function MyApp({ Component, pageProps }: AppProps & {Component: {Layout: FC} }) 
     // wrap components in [Layout] component if it is defined if not wrap in [Noop] Component
         const Layout = Component.Layout ?? Noop;
     // wrap components in [Layout] component if it is defined if not wrap in [Noop] Component
-    const ui = useUI();
-    console.log(ui);
     return (
         
         <UIProvider>
