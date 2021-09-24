6e27e05e874a9f971550b40a2c5d4577254ee1d2
fredwills9224 <willsf9224@gmail.com>
2021-09-24 09:19:21 -0400

    productView s.price tag applied

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -22,9 +22,9 @@ const ProductView: FC<Props> = ({product})=> {
                     <div className={s.nameBox}>
                         
                         <h1 className={s.name}>{product.name}</h1>
-                        <div>
+                        <div className={s.price}>
                             {product.price.value}
-                            {``}
+                            {` `}
                             {product.price.currencyCode}
                         </div>
 
