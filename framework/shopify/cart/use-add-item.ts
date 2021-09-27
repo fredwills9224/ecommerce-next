42d717ee1cc11e4613904871d74d378aac6dec4e
fredwills9224 <willsf9224@gmail.com>
2021-09-27 06:29:59 -0400

    pass custom fetch

--- framework/shopify/cart/use-add-item.ts
+++ framework/shopify/cart/use-add-item.ts
@@ -5,16 +5,19 @@ export default useAddItem;
 
 export const handler: MutationHook = {
 
-    fetcher: (input: any)=>{
-        return JSON.stringify(input)+ '_MODIFIED'
+    fetcher: ({fetch, input})=>{
+        const response = fetch(input);
+        return response;
     },
     useHook: ({fetch})=>{
 
         return (input: any)=>{
+
             const response = fetch(input);
             return{
                 output: response
             };
+            
         };
 
     }
