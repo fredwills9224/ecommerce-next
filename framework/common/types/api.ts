51166325e3566ad098a7bfab7e0922cfc0fc2b6c
fredwills9224 <willsf9224@gmail.com>
2021-09-27 16:58:44 -0400

    fixing types

--- framework/common/types/api.ts
+++ framework/common/types/api.ts
@@ -1,3 +1,5 @@
+import { ApiHooks } from "./hooks";
+
 export type ApiFetcherOptions = {
     query: string,
     variables?: Variables
@@ -13,12 +15,7 @@ export interface ApiConfig{
     fetch<T>(options: ApiFetcherOptions): Promise<ApiFetcherResults<T>>
 };
 
-export interface ApiHooks {
-    cart: {
-        useAddItem: any,
-        useCart: any
-    }
-};
+
 
 export type ApiFetcher<T = any> = 
     (options: ApiFetcherOptions)=> Promise<ApiFetcherResults<T>>
--- framework/common/utils/use-hook.ts
+++ framework/common/utils/use-hook.ts
@@ -1,6 +1,5 @@
 import { useApiProvider } from "@common";
-import { ApiHooks } from "@common/types/api";
-import { MutationHook } from "@common/types/hooks";
+import { MutationHook, ApiHooks } from "@common/types/hooks";
 
 export const useHook = (fn: (apiHooks: ApiHooks)=> MutationHook)=>{
     const { hooks } = useApiProvider();
