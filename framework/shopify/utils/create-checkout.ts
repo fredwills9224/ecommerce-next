94558984bfb28e5e82b7ce514406c9a22a30fe51
fredwills9224 <willsf9224@gmail.com>
2021-09-28 13:24:32 -0400

    checkout create mutation

--- framework/shopify/utils/create-checkout.ts
+++ framework/shopify/utils/create-checkout.ts
@@ -1,5 +1,15 @@
-const createCheckout = ()=>{
-    return {data: 'checkout created!'};
+import { ApiFetcher } from "@common/types/api";
+import { checkoutCreateMutation } from "./mutations";
+
+const createCheckout = async (fetch: ApiFetcher): Promise<any> =>{
+    
+    debugger;
+    const { data } = await fetch({
+        query: checkoutCreateMutation
+    });
+    debugger;
+    return data;
+
 };
 
 export default createCheckout;
