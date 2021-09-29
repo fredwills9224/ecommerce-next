b4ede04ed0a76b2d580aa93da647a3e779bd3be1
fredwills9224 <willsf9224@gmail.com>
2021-09-29 13:56:54 -0400

    useAddItem type cl-up

--- components/product/ProductView/ProductView.tsx
+++ components/product/ProductView/ProductView.tsx
@@ -30,8 +30,6 @@ const ProductView: FC<Props> = ({product})=> {
                 quantity:1
             };
             const output = await addItem(item);
-            debugger;
-            alert(JSON.stringify(output));
             openSidebar();
 
         }catch{}
