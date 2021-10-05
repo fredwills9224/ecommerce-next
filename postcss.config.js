90c9e2f18bdbdec659a9415a80cd49160d7d02ee
fredwills9224 <willsf9224@gmail.com>
2021-10-05 15:55:48 -0400

    postcss + sidbar fx

--- postcss.config.js
+++ postcss.config.js
@@ -1,36 +1,12 @@
 module.exports = {
+
     plugins: [
-      
         'tailwindcss',
         'postcss-nesting',
-        'postcss-flexbugs-fixes',
-        [
-
-            'postcss-preset-env',
-            {
-                autoprefixer: {
-                    flexbox: 'no-2009',
-                },
-                stage: 3,
-                features: {
-                    'custom-properties': false,
-                }
-            }
-
-        ]
-
+        'autoprefixer'
     ]
-}
 
-// module.exports = {
-
-//     plugins: [
-//         'tailwindcss',
-//         'postcss-nesting',
-//         'autoprefixer'
-//     ]
-
-// };
+};
 
 // just for testing!
 // module.exports = {
