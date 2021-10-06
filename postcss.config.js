9a82fda665448ed031e3996e78f935b339f948b7
fredwills9224 <willsf9224@gmail.com>
2021-10-06 09:51:54 -0400

    another post css config upd

--- postcss.config.js
+++ postcss.config.js
@@ -1,12 +1,32 @@
+const purgecss = [
+  "@fullhuman/postcss-purgecss",
+  {
+    content: ["./components/**/*.js", "./pages/**/*.js"],
+    defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
+  },
+];
+
 module.exports = {
-
-    plugins: [
-        'tailwindcss',
-        'postcss-nesting',
-        'autoprefixer'
-    ]
-
+  plugins: [
+    "postcss-import",
+    "tailwindcss",
+    "autoprefixer",
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : []),
+  ],
 };
+  
+  
+// workin with problems
+    // module.exports = {
+
+    //     plugins: [
+    //         'tailwindcss',
+    //         'postcss-nesting',
+    //         'autoprefixer'
+    //     ]
+
+    // };
+// workin with problems
 
 // just for testing!
 // module.exports = {
